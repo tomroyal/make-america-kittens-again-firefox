94da40f0fae43aba4cfcf395ba9065b9d27732c4
Tom Royal <tom@tomroyal.com>
2018-01-07 13:18:10 +0000

    initial working version

---
+++ README.md
@@ -0,0 +1,12 @@
+# Make-America-Kittens-Again
+
+This is Make America Kittens Again:
+
+https://github.com/tomroyal/Make-America-Kittens-Again
+
+https://chrome.google.com/webstore/detail/make-america-kittens-agai/klchnmggepghlcolikgaekpibclpmgcm
+
+.. that replaces images of Donald Trump with kittens, reworked a bit to support Firefox Quantum. It's currently missing a few bells and whistles but mostly works.
+
+@tomroyal
+tomroyal.com
---
+++ setmakamenu.js
@@ -0,0 +1,23 @@
+// setmakamenu.js - part of make america kittens again
+// v1.1.3
+// by Tom Royal
+// tomroyal.com
+
+// sets up a menu item to invoke the de-kittening
+/*
+function DeMakaMenuClick() {
+  console.log("trying");
+  for (let tab of tabs) {
+    browser.tabs.sendMessage(
+      tab.id,
+      {message: "demaka"}
+    ).then(response => {
+      console.log("Message from the content script:");
+      console.log(response.response);
+    }).catch(onError);
+  }
+}
+
+
+var setMAKAmenu1 = browser.contextMenus.create({"title": "Make Kittens Trump Again :(", "contexts":["page"], "onclick": DeMakaMenuClick});
+*/
